60139cff78f386088ee523ebf2fe24ca2b0f20af
Ryan Ridden <rri38@MacBook-Pro-2.canterbury.ac.nz>
2022-11-30 10:14:19 +1300

    comment added

--- otehiwai_po/otehiwai_po.py
+++ otehiwai_po/otehiwai_po.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     make_debass_list()
     make_swope_list()
     make_schedule()
-
+    
+    print('!!! pushing to Git !!!')
     os.system('git add *')
     os.system('git commit -m "update"')
     os.system('git push')
