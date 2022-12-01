5e1c1d42b522ce7bff00b8326a008b8d565e960d
Ryan Ridden <rri38@MacBook-Pro-2.canterbury.ac.nz>
2022-12-02 11:25:26 +1300

    update

--- otehiwai_po/otehiwai_po.py
+++ otehiwai_po/otehiwai_po.py
@@ -6,6 +6,8 @@ import os
 
 
 if __name__ == '__main__':
+    print('!!! pulling from Git !!!')
+    os.system('git pull')
     make_look_list(name_priority=[['81P',1],['73P',1],['UN271',1]],mag_priority=[['22-19',3],['19-17',4],['17-15',5],['15-12',6]])
     make_debass_list()
     make_swope_list()
