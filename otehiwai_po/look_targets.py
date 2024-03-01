8cb069255e2662fb6701c644cba4a00c26184bf5
Ryan Ridden <rri38@Beep-booper.local>
2024-03-01 19:13:11 +1300

    old

--- otehiwai_po/look_targets.py
+++ otehiwai_po/look_targets.py
@@ -134,4 +134,4 @@ def make_look_list(name_priority,mag_priority):
 
 
 if __name__ == '__main__':
-    make_look_list(name_priority=[['81P',1],['73P',1],['UN271',1]],mag_priority=[['22-19',3],['19-17',4],['17-15',5],['15-12',6]])
+    make_look_list(name_priority=[['358P',1],['UN271',1]],mag_priority=[['22-19',3],['19-17',4],['17-15',5],['15-12',6]])
